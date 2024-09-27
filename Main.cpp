7d8c14bfdf19f8ac038fe00cd7da381a81c2f98c
jan karlo <leossuarez103@gmail.com>
2024-09-26 18:36:56 -0600

    agrego la informacion de las nuevas probs

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,18 @@
 #include "Academico.h"
+#include "Usuarios.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
-	Academico academico("Maria", "987654321", "maria@example.com");
+	//creamos un usuario academico
+	Academico academico("Maria", "987654321", "maria@example.com", "Doctor en matematicas aplicadas", "matematicas");
+	//creamos un usuario estudiante
+	Usuario usuario("juan", "3322116655", "juan@escolar,com","licenciatura en ciencias de la cumuncacion","44774477414");
 	
 	
+	//mostramos nuestros usuarios
 	academico.mostrarDatos();
+	usuario.mostrarDatos();
 	
 	}
