a16ddc23e963dec6441e8438a7685221a2ffa9a6
jankarli <jankarli69@outlook.com>
2024-09-27 23:32:22 -0600

    ya muestra a usuario y usuario academico

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,14 @@
 #include "Academico.h"
-#include "Usuarios.h"
+#include "Usuario.h"
 #include <iostream>
 
 using namespace std;
 
 int main(){
 	//creamos un usuario academico
-	Academico academico("Maria", "987654321", "maria@example.com", "Doctor en matematicas aplicadas", "matematicas");
+	Academico academico("Maria", "987654321", "maria@example.com", "Doctor", "matematicas");
 	//creamos un usuario estudiante
-	Usuario usuario("juan", "3322116655", "juan@escolar,com","licenciatura en ciencias de la cumuncacion","44774477414");
+	Usuario usuario("juan", "3322116655", "juan@escolar,com","ciencias de la cumuncacion", "44774477414");
 	
 	
 	//mostramos nuestros usuarios
