e0091eb2e57ba421401949f5ee6b96ecbaa2c9a9
jankarli <jankarli69@outlook.com>
2024-09-28 17:42:48 -0600

    se declaran las propiedade y metodos

---
+++ Libro.h
@@ -0,0 +1,36 @@
+#ifndef LIBBRO_H
+#define LIBRO_H
+
+#include <iostream>
+#include <string>
+#include <ctime>
+
+
+using namespace std;
+
+//Super clase de usuario
+class Libro{
+	//atributos
+	private:
+		string titulo;
+		string autor;
+		bool prestado;
+		time_t fechaDevolucion;
+	
+		
+		
+	//metodos
+	public:
+		Libro(string titulo, string autor);
+		
+		void prestarLibro(int dias);
+		void devolverLibro();
+		bool estadoLibro();
+		time_t tiempoRestante();
+		
+		// Métodos para acceder a los atributos privados
+		string getTitulo() const {return titulo; }
+		string getAutor() const {return autor; }
+			 
+	};
+#endif
