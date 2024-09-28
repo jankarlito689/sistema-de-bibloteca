7d21c8e985d358428730110ce37c0404dbd7514a
jankarli <jankarli69@outlook.com>
2024-09-28 17:39:57 -0600

    se crea libro

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include "Academico.h"
 #include "Usuario.h"
+#include "Libro.h"
 #include <iostream>
 
 using namespace std;
@@ -10,6 +11,8 @@ int main(){
 	//creamos un usuario estudiante
 	Usuario usuario("juan", "3322116655", "juan@escolar,com","ciencias de la cumuncacion", "44774477414");
 	
+	 // Crear algunos libros
+	 Libro libro("Clean Code", "Robert C. Martin");
 	
 	//mostramos nuestros usuarios
 	academico.mostrarDatos();
