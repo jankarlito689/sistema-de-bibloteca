1ff0f899e1e0833b8bbddff7021eb282c3786271
jankarli <jankarli69@outlook.com>
2024-09-30 22:42:14 -0600

    mostramos la clase libro

--- Main.cpp
+++ Main.cpp
@@ -8,14 +8,16 @@ using namespace std;
 int main(){
 	//creamos un usuario academico
 	Academico academico("Maria", "987654321", "maria@example.com", "Doctor", "matematicas");
+	cout<<"";
 	//creamos un usuario estudiante
 	Usuario usuario("juan", "3322116655", "juan@escolar,com","ciencias de la cumuncacion", "44774477414");
-	
+	cout<<""<<endl;
 	 // Crear algunos libros
 	 Libro libro("Clean Code", "Robert C. Martin");
 	
 	//mostramos nuestros usuarios
 	academico.mostrarDatos();
 	usuario.mostrarDatos();
+	libro.mostrarDatos();//mostramos libro
 	
 	}
