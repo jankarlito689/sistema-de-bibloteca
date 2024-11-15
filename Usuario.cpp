8a344531e68739094fbd86354b6767ebd5eea4d6
cesar jan karlo <leossuarez103@gmail.com>
2024-11-14 22:30:56 -0600

    se ya funciona los metodos agregarAlHistorial y HistorialLibro

--- Usuario.cpp
+++ Usuario.cpp
@@ -4,3 +4,20 @@
 Usuario::Usuario(string nombre, string telefono, string email, string carrera, string codigoEstudiante)
 	:nombre(nombre), telefono(telefono), email(email), carrera(carrera), codigoEstudiante(codigoEstudiante){}
 
+// Agrega un libro al historial del usuario
+void Usuario::agregarAlHistorial(const string& titulo){
+	historialLibros.push_back(titulo);
+	}
+
+// Muestra el historial de libros del usuario
+void Usuario::HistorialLibros() const{
+		if (historialLibros.empty())
+		{
+			cout << nombre << " no tiene libros en su historial." << endl;
+		}else{
+			cout << "Historial de libros de " << nombre << ":" << endl;
+			for (const string& libro : historialLibros) {
+				cout << "- " << libro << endl;
+			}
+		}
+	}
