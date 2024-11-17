a142879556226635f1e0f9ccbaf1453c8f6a6a8b
cesar jan karlo <leossuarez103@gmail.com>
2024-11-17 12:45:10 -0600

    se instanciaron las sobrecargas de las funciones de informcion usuario y academico

--- Administrador.h
+++ Administrador.h
@@ -28,8 +28,10 @@ class Bibliotecario : public Usuario {
 			  void eliminarAcademico(vector<Academico*>& academicos, const string& nombre);
 			  
 			  //Métodos para mostrar la informacion de usuario y academico
-			  void informacioUsuario(vector<Usuario*>& usuarios);
-			  void informacionAcademico(vector<Academico*>& academicos);
+			  void informacioUsuario(vector<Usuario*>& usuarios); // Muestra todos los usuarios
+			  void informacioUsuario(Usuario* usuario); // Muestra un solo usuario
+			  void informacionAcademico(vector<Academico*>& academicos); // Muestra todos los académicos
+			  void informacionAcademico(Academico* academico);  // Muestra un solo académico
 			  void mostraInformacion(vector<Usuario*>& usuarios, vector<Academico*>& academicos);
 			  void mostraHistorial(vector<Usuario*>& usuarios, vector<Academico*>& academicos);
 			  void mostraSubmenu(vector<Usuario*>& usuarios, vector<Academico*>& academicos, vector<Libro>& libros, Usuario* usuario);
